1f9b4efb3e43529c38b123c9cf43bd4d6b5ffb25
mervet-robot <mervetrobot@gmail.com>
2025-05-14 03:25:37 +0200

    first commit

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+const API_URL = 'http://localhost:8081/api/test/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) { }
+
+  getPublicContent(): Observable<any> {
+    return this.http.get(API_URL + 'all', { responseType: 'text' });
+  }
+
+
+  getResponsableBoard(): Observable<any> {
+    return this.http.get(API_URL + 'responsable', { responseType: 'text' });
+  }
+
+  getDirecteurBoard(): Observable<any> {
+    return this.http.get(API_URL + 'directeur', { responseType: 'text' });
+  }
+}
